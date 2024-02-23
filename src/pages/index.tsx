ab7755ae59da895c66bed1da66b9af8cd9b689d0
Swaroop Kovoor <jakekovoor@gmail.com>
2024-02-23 21:44:27 +0800

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,63 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter } from "next/font/google"
+import Image from "next/image"
+import { useState } from "react"
+import TaxCalculator from "../components/TaxCalculator"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
+  useState
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-14 lg:p-24 ${inter.className}`}
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
+          Tax Tools (Open-Source)&nbsp;
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="lg:flex flex-row gap-2 justify-center align-middle items-center mt-10 sm:mt-0 hidden">
+          Built in public by{" "}
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="https://twitter.com/jakekovoor"
             target="_blank"
-            rel="noopener noreferrer"
+            className="flex justify-center bg-white font-bold p-2 rounded-2xl items-center gap-1 border-b border-gray-300 bg-gradient-to-b from-zinc-200  backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200  lg:dark:bg-zinc-800/30"
           >
-            By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/profile-pic.jpg"
+              width={30}
+              height={10}
+              alt="Picture of the developer"
+              className="rounded-full"
             />
+            @jakekovoor
           </a>
         </div>
       </div>
+      <h1 className=" mt-10 text-lg font-bold font-mono underline max-w-xl text-center">
+        LHDN Tax Calculator - How to Calculate (CP502) Increase in Tax If the Amended Estimated Tax
+        Is Lower
+      </h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <TaxCalculator />
+      <div className="border-b border-gray-300 backdrop-blur-2xl dark:border-neutral-800" />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="grid text-center lg:max-w-2xl lg:w-full lg:mb-0  lg:text-left">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="https://phl.hasil.gov.my/pdf/pdfam/CP502_NOTA_PENERANGAN_2.pdf"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
+            Official Source{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>CP502 Explanation (PDF) - LHDN</p>
         </a>
 
-        <a
+        {/* <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -111,8 +106,25 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
+        </a> */}
+      </div>
+      <div className="flex flex-row gap-2 justify-center align-middle items-center mt-10 sm:mt-0 lg:hidden font-mono text-xs">
+        Built in public by{" "}
+        <a
+          href="https://twitter.com/jakekovoor"
+          target="_blank"
+          className="flex justify-center bg-white font-bold p-2 rounded-2xl items-center gap-1 border-b border-gray-300     backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200  lg:dark:bg-zinc-800/30"
+        >
+          <Image
+            src="/profile-pic.jpg"
+            width={30}
+            height={10}
+            alt="Picture of the developer"
+            className="rounded-full"
+          />
+          @jakekovoor
         </a>
       </div>
     </main>
-  );
+  )
 }
