c14c89b04812a94b931ff60a14f92d8b36ab5e8a
Swaroop Kovoor <jakekovoor@gmail.com>
2024-02-24 00:13:06 +0800

    x

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+
+import { Metadata } from "next"
+
+// either Static metadata
+export const metadata: Metadata = {
+  verification: {
+    google: "aWF77SEz5IwWJG1DXw6JZ_LkFIIkVFWh_3LQNKvjtNQ",
+  },
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,5 @@
 import { Head, Html, Main, NextScript } from "next/document"
 
-import { Metadata } from "next"
-
-// either Static metadata
-export const metadata: Metadata = {
-  verification: {
-    google: "aWF77SEz5IwWJG1DXw6JZ_LkFIIkVFWh_3LQNKvjtNQ",
-  },
-}
-
 export default function Document() {
   return (
     <Html lang="en">
