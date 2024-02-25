680dba6b61ed50de78b1ddcdd11d130e7852d133
Swaroop Kovoor <jakekovoor@gmail.com>
2024-02-26 01:21:41 +0800

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
+import Script from "next/script"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        id="google-analytics-script"
+      />
+
+      <Script strategy="lazyOnload" id="google-analytics-config">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
